3f71eacd53fab5be42df6738e4db6e37c475d07f
violaceous <bodhisnatcher@gmail.com>
2014-11-13 19:11:04 -0500

    this is as far as I am taking the experiment in trying to use jquery for hitting neo4j from casper for now. new plan is to spawn casper scripts using python instead

--- data/neo4jtest.js
+++ data/neo4jtest.js
@@ -1,22 +1,45 @@
 
-var casper = require('casper').create();
-
-
-casper.userAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36');
-
-function getMovieDetails() {
-    casper.echo('movie details go here');
-}
-
+var casper = require('casper').create({
+});
 
 casper.start("www.google.com", function() {
+
 });
 
-casper.then(function () {
-    getMovieDetails();
+casper.then(function() {
+    casper.page.injectJs('jquery.js');
+    casper.echo('injected jquery');
+    var result = casper.evaluate(function() {
+	var toReturn = "aaa";
+	$.ajax({
+	    type:"POST",
+	    url: "http://localhost:7474/db/data/transaction/commit",
+	    accept: "application/json; charset=UTF-8",
+	    contentType:"application/json",
+	    headers: { 
+		"X-Stream": "true"    
+	    },
+	    data: JSON.stringify({
+
+		"statements" : [ {
+		    "statement" : "CREATE (n) RETURN id(n)"
+		} ]
+
+	    }),
+	    success: function(data, textStatus, jqXHR){
+		toReturn = "success:" + textStatus;
+	    },
+	    error: function(jqXHR, textStatus, errorThrown){
+		toReturn =  "error:" + textStatus;
+	    }
+	});//end of ajax
+
+
+	return setTimeout(function(){return toReturn}, 5000);
+
+    });
+    casper.echo("result: " + result);
 });
-
-
 
 casper.run(function() {
 });
