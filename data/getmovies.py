5da080feb2869663bc0d4abc9469746557420652
violaceous <bodhisnatcher@gmail.com>
2014-12-08 10:12:15 -0500

    added thread timeout/restarting for gettting movies

--- data/getmovies.py
+++ data/getmovies.py
@@ -1,38 +1,125 @@
 import subprocess
+from subprocess import Popen, STDOUT, PIPE
+import csv
+import StringIO
+import time
+import threading
+import signal
+import os
+import time
 from py2neo import cypher
 
-def splitLinks(links):
-    links = links.split('\n')
-    for link in links:
-        if len(link) > 0:
-            processLink(link)
+count = 0;
+movie_file = open("movies.txt","a")
+database_errors = open("movies_database_errors.txt","a")
+dead_threads = 0
+finished_pages = 0
 
-def processLink(link):
-    splitLink = link.split('/')
-    # this is pretty awful looking - should be a better way to do this although it works
-    if len(splitLink) >= 6:
-        link = splitLink[0] + '/' + splitLink[1] + '/' + splitLink[2] + '/' + splitLink[3] + '/' + splitLink[4] + '/' + splitLink[5]
-        saveMovie(splitLink[5], link)
+def run_popen_with_timeout(command_string, timeout, input_data):
+    """
+    Run a sub-program in subprocess.Popen, pass it the input_data,
+    kill it if the specified timeout has passed.
+    returns a tuple of success, stdout, stderr
+    """
+    kill_check = threading.Event()
+    def _kill_process_after_a_timeout(pid):
+        os.kill(pid, signal.SIGTERM)
+        kill_check.set() # tell the main routine that we had to kill
+        # use SIGKILL if hard to kill...
+        global dead_threads
+        dead_threads = dead_threads + 1
+        return
+    p = Popen(command_string, shell=True, stdout=PIPE, stderr=PIPE)
+    pid = p.pid
+    watchdog = threading.Timer(timeout, _kill_process_after_a_timeout, args=(pid, ))
+    watchdog.start()
+    (stdout, stderr) = p.communicate(input_data)
+    watchdog.cancel() # if it's still waiting to run
+    success = not kill_check.isSet()
+    kill_check.clear()
+    return (success, stdout, stderr)
 
-def saveMovie(movieid, link):
+def createMovie(movieid):
+    statement = "CREATE (movie:MOVIE {amazon_id:{movieid}})"
+    movie_file.write("CREATE (movie:MOVIE {amazon_id: '" + movieid + "')\n")
     session = cypher.Session("http://localhost:7474")
     tx = session.create_transaction()
     try:
-        # would look better to do it propper like with the variables instead of a string
-        tx.append("CREATE (movie:MOVIE {amazon_link:'" + link + "',amazon_id:'" + movieid + "'})")
+        tx.append(statement, {"movieid": movieid})
         tx.commit()
-    except cypher.TransactionError:
-        # movie already existed and couldn't be created again. All is well, fail silently.
-        bob = 1
-#
-#f = open('someMovies.txt', 'r')
-#output = f.read()
-#print output
-#splitLinks(output)
+    except cypher.TransactionError as e:
+        pass
+        database_errors.write(e.message + "\n")
 
+def processMovie(row):
+    valid_movie = False
+    if len(row) == 6:
+        valid_movie = True
+        link = row[0]
+        title = row[1]
+        ratings = row[2]
+        year = row[3]
+        image = row[4]
+        rating = row[5]
+        link = link.split('/')
+        movieid = link[5]
+        link = link[0] + '/' + link[1] + '/' + link[2] + '/' + link[3] + '/' + link[4] + '/' + link[5]
+        createMovie(movieid)
 
-output = subprocess.check_output(['proxychains','casperjs','movies.js'])
-# print output
-splitLinks(output)
+        statement = "MATCH (movie:MOVIE {amazon_id:{movieid}}) SET movie += {amazon_link:{link}, title:{title}, year:{year}, image:{image}, rating:{rating}, ratings:{ratings}}"
+        session = cypher.Session("http://localhost:7474")
+        tx = session.create_transaction()
+        movie_file.write("MATCH (movie:MOVIE {amazon_id:'" + movieid + "') SET movie += {amazon_link: '" + link + "', title: '" + title + "', year: '" + year + "', image: '" + image + "', rating: '" + rating + "', ratings: '" + ratings + "'})\n")
+        try:
+            tx.append(statement, {"link": link, "movieid": movieid, "title": title, "year": year, "image": image, "rating": rating, "ratings": ratings})
+            tx.commit()
+        except cypher.TransactionError as e:
+            pass
+            database_errors.write(e.message + "\n")
+    return valid_movie
+
+start_time = time.clock() 
+start_dead_threads = 0
+elapsed_time = time.clock()
+# should be 1 through 400
+range_start = 1
+range_finish = 400
+for i in range(range_start, range_finish):
+    # this is just by newest
+    #url = "\'http://smile.amazon.com/s/ref=sr_pg_3?rh=n%3A7589478011%2Cn%3A7589479011&page=" + str(i) + "&sort=date-desc-rank&ie=UTF8\'"
+    # this is by new and popular
+    url = "\'http://smile.amazon.com/s/ref=sr_pg_3?rh=n%3A7589478011%2Cn%3A7589479011&page=" + str(i) + "&sort=popularity-rank&ie=UTF8\'"
+    print "Last paged finished in " + str((time.clock() - start_time) * 1000) + " seconds and restarted " + str(dead_threads - start_dead_threads) + " times"
+    print "Finished total of " + str(finished_pages) + ", " + str(dead_threads) + " threads restarted"
+    if finished_pages > 0:
+        time_per_page = ((time.clock() - elapsed_time) * 1000)/finished_pages
+    else:
+        time_per_page = 180
+    time_left = time_per_page * ((range_finish - range_start) - finished_pages)
+    print str(time_per_page) + " average seconds per page and " + str(time_left/60) + " minutes estimated until completion"
+    global start_time
+    start_time = time.clock()
+    global start_dead_threads
+    start_dead_threads = dead_threads
+    timeout = 60
+    command_string = "proxychains casperjs --web-security=false movies.js " + url
+    success = False
+    valid_result = False
+    while not success and not valid_result:
+        success, output, error = run_popen_with_timeout(command_string, timeout, "blah")
+        output = StringIO.StringIO(output)
+        csvreader = csv.reader(output, delimiter=',', quotechar='#')
+        for row in csvreader:
+            result = processMovie(row)
+            if not valid_result and result:
+                valid_result = True
+        if not success and not valid_result:
+            time.sleep(60)
+    finished_pages = finished_pages + 1
+    time.sleep(15)
+
+movie_file.close()
+database_errors.close()
+
 
  
--- data/movies.js
+++ data/movies.js
@@ -1,62 +1,112 @@
 var delay = 6000; // timeout between pages
 var links = [];
-//var url = 'http://smile.amazon.com/s/ref=s9_hps_bw_clnk_a_v?node=2858778011,7613704011&search-alias=prime-instant-video&sort=csrank&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=center-6&pf_rd_r=03D83JJV25XQ8CDPQVCW&pf_rd_t=101&pf_rd_p=1959989462&pf_rd_i=2858778011&pf_rd_p=1959989462&pf_rd_s=center-10&pf_rd_t=101&pf_rd_i=2858778011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=03D83JJV25XQ8CDPQVCW';
-var url = 'http://smile.amazon.com/s/ref=sr_st_date-desc-rank?qid=1416059713&rh=n%3A7589478011%2Cn%3A7589479011&sort=date-desc-rank';
+var title = [];
+var year = [];
+var image = [];
+var ratings = [];
+var rating = [];
+var movies = [];
+
+// use this one to keep up to date
+// var url = 'http://smile.amazon.com/s/ref=sr_st_date-desc-rank?qid=1416059713&rh=n%3A7589478011%2Cn%3A7589479011&sort=date-desc-rank';
+var url = 'http://smile.amazon.com/s/ref=sr_pg_3?rh=n%3A7589478011%2Cn%3A7589479011&page=5&sort=date-desc-rank&ie=UTF8';
 var fs = require('fs');
+var x = require('casper').selectXPath;
 var pages = "";
-
-
-
 var casper = require('casper').create();
-var maxLoops = 10000; // used to limit number of repeats for testing the script
+var maxLoops = 0; // used to limit number of repeats for testing the script
 
 casper.userAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36');
 
-function getLinks() {
-    var links = document.querySelectorAll('div > div.a-row.a-spacing-none > div:nth-child(1) > a');
-    return Array.prototype.map.call(links, function(e) {
-	return e.getAttribute('href');
+function getMovies() {
+    var elements = __utils__.getElementsByXPath('//*[@class="s-item-container"]');
+    return Array.prototype.map.call(elements, function(element) {
+        return element.innerHTML;
     });
 }
 
 function getMoviesAndWrite(){
-    casper.echo('in getMovies');
 
-    
+    /* to get the html
      var js = casper.evaluate(function() {
      return document; 
      });	
-     pages = pages + js.all[0].outerHTML; 
+     pages = pages + js.all[0].outerHTML;
+    */
     
+    movies = casper.evaluate(getMovies);
 
-    links = casper.evaluate(getLinks);
-    links = links.join("\n");
-    casper.echo(links);
+    for (var i = 0; i < movies.length; i++) {
+	var movie = document.createElement('div');
+	movie.innerHTML = movies[i];
+
+	links[i] = movie.querySelectorAll('div > div.a-row.a-spacing-none > div:nth-child(1) > a')[0].getAttribute('href');
+	title[i] = movie.querySelectorAll('div > div.a-row.a-spacing-none > div:nth-child(1) > a > h2')[0].innerHTML;
+
+	var curRatings = movie.querySelectorAll('div > div.a-row.a-spacing-none > div.a-row.a-spacing-top-mini.a-spacing-none > a')[0];
+	if(typeof(curRatings) !== 'undefined') {
+	    ratings[i] = curRatings.innerHTML;
+	} else {
+	    ratings[i] = "";
+	}
+
+	var curYear = movie.querySelectorAll('div > div.a-row.a-spacing-none > div:nth-child(1) > span.a-size-small.a-color-secondary')[0];
+	if(typeof(curYear) !== 'undefined') {
+	    year[i] = curYear.innerHTML;
+	} else {
+	    year[i] = "";
+	}
+
+	var curImage = movie.querySelectorAll('div > div.a-row.a-spacing-base > div > a > img')[0];
+	if(typeof(curImage) !== 'undefined') {
+	    image[i] = curImage.getAttribute('src');
+	} else {
+	    image[i] = "";
+	}
+	var curRating = movie.querySelectorAll('div > div.a-row.a-spacing-none > div:nth-child(1) > div:nth-child(5) > div > span.a-size-small.a-color-secondary')[0];
+	if(typeof(curRating) !== 'undefined' && curRating.innerHTML != 'CC') {
+	    rating[i] = curRating.innerHTML;
+	} else {
+	    rating[i] = "";
+	}
+
+    }
+
+
+
+    for (var i = 0; i < links.length; i++) {
+	casper.echo('#' + links[i] + '#,#' + title[i] + '#,#' + ratings[i] + '#,#' + year[i] + '#,#' + image[i] + '#,#' + rating[i] + '#');
+    }
+
 
     var nextLink = "#pagnNextLink";
 
     if (casper.visible(nextLink) && maxLoops > 0) {
 	maxLoops = maxLoops - 1;
-	casper.wait('6000', function() {
+	self.wait(6000, function() {
 	    casper.thenClick(nextLink);
-	    casper.wait(10000, function() {
+	    self.wait(10000, function() {
 		casper.then(getMoviesAndWrite);
 	    });
 	});
     } else {
-	// fs.write('pagehtml.txt',pages,'w');
+	this.exit();
     }
 }
 
-casper.start(url, function() {
+
+casper.start('www.google.com', function() {
+    url = casper.cli.get(0).trim();
 });
 
-casper.then(function () {
+casper.then(function() {
+    casper.wait(6000, function() {
+	this.open(url);
+    });
+});
+
+casper.then(function() {
     getMoviesAndWrite();
-});
-
-casper.then(function () {
-    this.exit();
 });
 
 casper.run(function() {
